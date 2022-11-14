bf26fb57ccaa90e2de5f6b28861ddbf148d7ac0a
Camila munoz <cmunoz@bpo.cl>
2022-11-14 08:40:59 -0300

    formularios_basicos

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask
-
-from flask import request
-
-from flask import render_template
-
-
-
+from flask import Flask, request, render_template, url_for, flash, redirect
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = 'Escribe cualquier cosa'
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
 @app.route('/')
-def hello():
-    return 'Hola Mundo'
-
+def index():
+    return render_template('index.html', messages = messages)
+@app.route('/create', methods = ('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+        if not title:
+            flash('El título es obligatorio')
+        elif not content:
+            flash('El contenido es obligatorio')
+        else:
+            messages.append({'title': title, 'content':content})
+            return redirect(url_for('index'))
+    return render_template('create.html')
 @app.route('/usuario/<name>')
 def user(name):
-    return render_template("user.html", name = name)
-
+    return render_template('user.html', user = name)
+@app.route('/usuario')
+def user_incognito():
+    return render_template('user.html')
 @app.route('/navegador')
 def browser():
-    user_agent = request.headers.get("User-Agent")
-    return f"tu navegador es:{user_agent}"
-
+    user_agent = request.headers.get('User-Agent')
+    return f'Tu navegador es: {user_agent}'
 @app.route('/rutas')
 def routes():
     print(app.url_map)
-    return "revisa tu consola para ver las rutas"
+    return 'Revisa tu consola para ver las rutas'
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 400    
