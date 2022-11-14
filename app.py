eb62ff45f60ab7c9f11d672f056dd5908ff63742
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2022-11-14 11:01:08 -0300

    half database

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
 from flask import Flask, request, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Escribe cualquier cosa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sbstn:ruby@localhost:5432/flask_bootstrap'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Message(db.Model):
+     __table__ = 'messages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'<Message {self.title}>'
+
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
             {'title': 'Message Two',
              'content': 'Message Two Content'}
             ]
+
 @app.route('/')
 def index():
     return render_template('index.html', messages = messages)
+
 @app.route('/create', methods = ('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -22,20 +42,25 @@ def create():
             messages.append({'title': title, 'content':content})
             return redirect(url_for('index'))
     return render_template('create.html')
+
 @app.route('/usuario/<name>')
 def user(name):
     return render_template('user.html', user = name)
+
 @app.route('/usuario')
 def user_incognito():
     return render_template('user.html')
+
 @app.route('/navegador')
 def browser():
     user_agent = request.headers.get('User-Agent')
     return f'Tu navegador es: {user_agent}'
+
 @app.route('/rutas')
 def routes():
     print(app.url_map)
     return 'Revisa tu consola para ver las rutas'
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 400    
